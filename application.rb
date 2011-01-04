3ac3c3dad4bd67fb85d3bb26195f3e8afb269f55
tbuehlmann <tobias.buehlmann@gmx.de>
2011-01-04 15:09:19 +0100

    removed use of pathname

--- application.rb
+++ application.rb
@@ -2,30 +2,30 @@
 
 require 'rubygems'
 require 'haml'
-require 'pathname'
 require 'sinatra/base'
 
 class Application < Sinatra::Base
   configure do
     enable :static
-    
-    set :views, Pathname.new(__FILE__).dirname.join('views').expand_path
-    set :public, Pathname.new(__FILE__).dirname.join('public').expand_path
+
+    set :views,  File.join(File.dirname(__FILE__), 'views')
+    set :public, File.join(File.dirname(__FILE__), 'public')
   end
-  
+
   helpers do
     # ...
   end
-  
+
   not_found do
-    haml '404'
+    haml 'Not found'
   end
-  
+
   error do
     haml "Error (#{request.env['sinatra.error']})"
   end
-  
+
   get '/' do
     haml :index
   end
 end
+
